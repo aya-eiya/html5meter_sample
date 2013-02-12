69534c82f6e745b2fe2f2d78413a1af82f4d26e8
Hidetoshi Ayabe <lionehar@infoseek.jp>
2013-02-12 23:29:46 +0900

    オートアニメーションするようにしたった

--- javascripts/meter.js
+++ javascripts/meter.js
@@ -1,6 +1,7 @@
 // (c) aya_eiya - 2013
 (function (){
-    this.setMeter = function(_id,_size,imageSrc,_percent){
+  this.setMeter = function(_id,_size,imageSrc,_percent,_isAutoAnimation){
+    var isAutoAnimation = (_isAutoAnimation)?true:false;
     var percent = _percent;
     var size = _size;
     var canvas = document.createElement('canvas');
@@ -11,98 +12,115 @@
     var transparent = "rgba(0,0,0,0)";
     var meterColor = "rgb(255,128,128)";
     var meterWidth = 16;
+    var autoAnimationSpeed = 500; // 1 - 1000
     var digs = {0:0,90:Math.PI/2,180:Math.PI,360:Math.PI*2};
 
     var margin = 0;
     var setMargin = function(){
-	margin = size * 0.1;
-	canvas.width = size + margin * 2;
-        canvas.height = size + margin * 2;
+      margin = size * 0.1;
+      canvas.width = size + margin * 2;
+      canvas.height = size + margin * 2;
     };
 
     var setFrameOfMeter = function(){
-	context.strokeStyle = meterColor;
-	context.lineWidth = 2;
-	context.beginPath();
-	context.arc(size/2 + margin,size/2 + margin, size/2 - meterWidth,digs[0],digs[360]);
-	context.stroke();
-	context.beginPath();
-	context.arc(size/2 + margin,size/2 + margin, size/2,digs[0],digs[360]);
-	context.stroke();
+      context.strokeStyle = meterColor;
+      context.lineWidth = 2;
+      context.beginPath();
+      context.arc(size/2 + margin,size/2 + margin, size/2 - meterWidth,digs[0],digs[360]);
+      context.stroke();
+      context.beginPath();
+      context.arc(size/2 + margin,size/2 + margin, size/2,digs[0],digs[360]);
+      context.stroke();
     };
 
     var setMerterValue = function(){
-	context.strokeStyle = meterColor;
-	context.lineWidth = meterWidth;
-	context.beginPath();
-	var lDig = -percent/100 * digs[180] + digs[90];
-	var rDig =  percent/100 * digs[180] + digs[90];
-	context.arc(size/2 + margin,size/2 + margin,size/2 - meterWidth/2, lDig,rDig);
-	context.stroke();
+      var tmpPercent = (isAutoAnimation)?0:percent;
+      var onTimeout = function(){
+        tmpPercent += 5;
+        if(tmpPercent > percent) tmpPercent = percent;
+        context.strokeStyle = meterColor;
+        context.lineWidth = meterWidth;
+        context.beginPath();
+        var lDig = -tmpPercent/100 * digs[180] + digs[90];
+        var rDig =  tmpPercent/100 * digs[180] + digs[90];
+        context.arc(size/2 + margin,size/2 + margin,size/2 - meterWidth/2, lDig,rDig);
+        context.stroke();
+        if(isAutoAnimation) setTextArea(); // 妥協
+        if(tmpPercent < percent) setTimeout(onTimeout,1000/autoAnimationSpeed);
+      };
+      onTimeout();
     };
 
     var setImageToCercle = function(){
-	var aspect = image.width/image.height;
-	context.strokeStyle = transparent;
-	context.beginPath();
-	context.arc(size/2 + margin,size/2 + margin, size/2 - meterWidth,digs[0],digs[360]);
-	context.stroke();
-	context.save();
-	context.clip();
-	context.drawImage(image,margin,margin,size * aspect,size);
-	context.restore();
+      var aspect = image.width/image.height;
+      context.strokeStyle = transparent;
+      context.beginPath();
+      context.arc(size/2 + margin,size/2 + margin, size/2 - meterWidth,digs[0],digs[360]);
+      context.stroke();
+      context.save();
+      context.clip();
+      context.drawImage(image,margin,margin,size * aspect,size);
+      context.restore();
     };
 
     var setTextArea = function(){
-	var fontSize = size * 0.18;
-	context.font = fontSize + "px Arial";
-	var text = percent+"%";
-	var textSize = context.measureText(text);
-	var textLeft = size * 0.9;
-	context.strokeStyle = meterColor;
-	context.fillStyle = "white";
-	context.lineWidth = 4;
-	context.beginPath();
-	context.save();
-	context.arc(textLeft,size - fontSize/2,textSize.width/2+5,0,digs[360]);
-	context.fill();
-	context.stroke();
-	context.clip();
-	context.beginPath();
-	context.fillStyle = "black";
-	context.fillText(text,textLeft-textSize.width/2,size-fontSize/8);
-	context.restore();
+      var fontSize = size * 0.18;
+      context.font = fontSize + "px Arial";
+      var text = percent+"%";
+      var textSize = context.measureText(text);
+      var textLeft = size * 0.9;
+      context.strokeStyle = meterColor;
+      context.fillStyle = "white";
+      context.lineWidth = 4;
+      context.beginPath();
+      context.save();
+      context.arc(textLeft,size - fontSize/2,textSize.width/2+5,0,digs[360]);
+      context.fill();
+      context.stroke();
+      context.clip();
+      context.beginPath();
+      context.fillStyle = "black";
+      context.fillText(text,textLeft-textSize.width/2,size-fontSize/8);
+      context.restore();
     };
 
     var ready = false;
 
+    var setAutoAnimationStatus = function(){
+      if(isAutoAnimation){
+        autoAnimationSpeed = (autoAnimationSpeed < 1)?1:autoAnimationSpeed;
+        autoAnimationSpeed = (autoAnimationSpeed > 1000)?1000:autoAnimationSpeed;
+      }
+    };
+
     var setUp = function(){
-	setMargin();
-        setImageToCercle();
-        setFrameOfMeter();
-        setMerterValue();
-        setTextArea();
-	ready = true;
+      setAutoAnimationStatus();
+      setMargin();
+      setImageToCercle();
+      setFrameOfMeter();
+      setMerterValue();
+      setTextArea();
+      ready = true;
     };
 
     image.onload = setUp;
     var setImage = function(imageSrc){
-	ready = false;
-	image.src = imageSrc;
+      ready = false;
+      image.src = imageSrc;
     };
     var setPercentage = function(_percent){
-	percent = _percent;
-	if(ready) setUp();
+      percent = _percent;
+      if(ready) setUp();
     };
 
     var setSize = function(_size){
-	console.log(_size);
-	size = _size;
-	if(ready) setUp();
+      console.log(_size);
+      size = _size;
+      if(ready) setUp();
     };
 
     setImage(imageSrc);
 
     return {setSize:setSize,setImage:setImage,setPercentage:setPercentage};
-    };
+  };
 })();
